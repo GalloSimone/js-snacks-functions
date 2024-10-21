81586be3a37b5b8039b13aced680b5be2b138168
GalloSimone <gallosimonecz@gmail.com>
2024-10-21 16:57:27 +0200

    end esercizio 1

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -7,7 +7,24 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 
-// Invoca la funzione qui e stampa il risultato in console
 
-
-//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+/**
+ * given an array of names I return a new array of the first letters of the previous array
+ * @param {Array} originalArray the complete array
+ * @returns {Array} an array of the first letters of the elements of the original array
+ */
+    function firstLetterArray(originalArray) {
+      // *nuovo array vuoto
+      const fistLetterArray = [];
+      // * per ogni elemento dell'array originale pusho la prima lettera dell'elemento corrente nel nuovo array
+      originalArray.forEach((currentWord) =>
+        fistLetterArray.push(currentWord.charAt(0))
+      );
+      return fistLetterArray;
+    }
+    
+    // Invoca la funzione qui e stampa il risultato in console
+    
+    const newArray = firstLetterArray(names);
+    console.log(newArray);
+    //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
