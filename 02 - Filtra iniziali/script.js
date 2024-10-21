f0e6133270d8874dad6e9df46f4181850ee93868
GalloSimone <gallosimonecz@gmail.com>
2024-10-21 17:14:36 +0200

    end esercizio 2

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -2,11 +2,28 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
+const searchedLetter="A"
+let newArray= [];
 
 // Dichiara la funzione qui.
-
+/**
+ * returns an array of array of the words in the original array that start with the searched letter
+ * @param {Array} array array to search in
+ * @param {Array} searchedLetter letter to search for
+ * @returns {Array} array of the words in the original array that start with the searched letter
+ */
+const arrowFirstLetter = (array, searchedLetter)=>{
+    const arrayFirstLetter =[];
+    array.forEach((currentItem)=>{
+        if (currentItem.charAt(0)=== searchedLetter) 
+            arrayFirstLetter.push(currentItem);
+    });
+    return arrayFirstLetter
+};
 
 // Invoca la funzione qui e stampa il risultato in console
+newArray = arrowFirstLetter(names, searchedLetter);
+console.log(newArray);
 
 
 
